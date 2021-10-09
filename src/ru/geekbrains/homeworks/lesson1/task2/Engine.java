547404eb9525136d9ad3926814c03401f860f0bd
Kosenko <Kosenko.2000@yandex.ru>
2021-10-09 14:51:11 +0500

    homework1

---
+++ src/ru/geekbrains/homeworks/lesson1/task2/Engine.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.homeworks.lesson1.task2;
+
+public class Engine {
+}
---
+++ src/ru/geekbrains/homeworks/lesson1/task3/Figure.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.homeworks.lesson1.task3;
+
+public interface Figure {
+
+    public void draw();
+
+    public double findSquare();
+
+}
