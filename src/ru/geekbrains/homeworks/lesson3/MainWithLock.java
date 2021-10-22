9d67cd2a08f714da3019b5bb1372ccacec19283d
Kosenko <Kosenko.2000@yandex.ru>
2021-10-22 20:10:45 +0500

    homework3 (final final version)

--- src/ru/geekbrains/homeworks/lesson3/MainWithLock.java
+++ src/ru/geekbrains/homeworks/lesson3/MainWithLock.java
@@ -1,9 +1,5 @@
 package ru.geekbrains.homeworks.lesson3;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MainWithLock {
 
@@ -11,21 +7,27 @@ public class MainWithLock {
     public static void main(String[] args) {
 
         Counter counter = new Counter();
-        try {
-            counter.increment();
-            System.out.println(counter.getCount());
-        } finally {
-            System.out.println("___");
-        }
 
-        Lock lock2 = new ReentrantLock();
-        lock2.lock();
-        try {
-            counter.increment();
-            System.out.println(counter.getCount());
-        } finally {
-            System.out.println("___");
-        }
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                counter.increment();
+                System.out.println(counter.getCount());
+                super.run();
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            @Override
+            public void run() {
+                counter.increment();
+                System.out.println(counter.getCount());
+                super.run();
+            }
+        };
+
+        thread1.start();
+        thread2.start();
 
     }
 }
